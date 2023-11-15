6e10399c6ec81763993333c10067efeb89aa6d26
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-11-15 09:01:47 +0500

    Add project files.

---
+++ Tyuiu.KoevaSJ.Sprint4.TaskReview.V26.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KoevaSJ.Sprint4.TaskReview.V26.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint4.TaskReview.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int n = 5;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "351268459614723";
+
+            double res = ds.Calculate(n, m, str);
+            double wait = 18432;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+
+/// 351
+/// 268
+/// 459
+/// 614
+/// 723
---
+++ Tyuiu.KoevaSJ.Sprint4.TaskReview.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoevaSJ.Sprint4.TaskReview.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoevaSJ.Sprint4.TaskReview.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00d17d61-e613-43aa-a4b7-3279b41bb6d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
